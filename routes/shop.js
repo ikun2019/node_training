69e7ff122a53887f8073a8e68289f9cf0de6d952
ikun <0215.miyatto@gmail.com>
2023-02-21 05:36:59 +0900

    トップページの作成

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-  
+  res.render('shop', {
+    pageTitle: 'Shop',
+    path: '/',
+    prods: []
+  });
 });
 
 module.exports = router;
