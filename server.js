855cee828ff3eec679474884128a4db22cc3bd4c
ikun <0215.miyatto@gmail.com>
2023-02-21 04:36:26 +0900

    ルーターの作成

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const dotenv = require('dotenv').config();
 // ! アプリの初期化
 const app = express();
 
+// ! ミドルウェアの使用
+app.use(express.urlencoded({ extended: false }));
+
+// !　ルーティング
+app.use('/', require('./routes/index'));
 
 // ! サーバーの待ち受け
 app.listen(process.env.PORT);
