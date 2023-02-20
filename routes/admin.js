e3823190082e912125398080207f2a7cbfed34b8
ikun <0215.miyatto@gmail.com>
2023-02-21 04:48:53 +0900

    404ページのルーティング作成

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+// * 商品追加ページ表示のルーティング
+router.get('/add-product', (req, res, next) => {
+
+});
+// ! 商品追加のルーティング
+router.post('/add-product', (req, res, next) => {
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 // !　ルーティング
-app.use('/', require('./routes/index'));
+app.use('/admin', require('./routes/admin'));
+app.use('/', require('./routes/shop'));
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not Found</h1>');
+});
 
 // ! サーバーの待ち受け
 app.listen(process.env.PORT);
