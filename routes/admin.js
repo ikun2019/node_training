a3e6265fdcbe6abc1485d3871d9dc1cae808b01a
ikun <0215.miyatto@gmail.com>
2023-02-21 05:28:01 +0900

    商品追加ページの作成

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 
-// * 商品追加ページ表示のルーティング
+// * 商品追加ページ表示
+// * /admin/add-product
 router.get('/add-product', (req, res, next) => {
-
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  })
 });
-// ! 商品追加のルーティング
+// ! 商品追加機能
 router.post('/add-product', (req, res, next) => {
 
 });
