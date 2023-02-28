b2ddbee52b0a62298c33f5de3aa9d6ed4e2f1ff2
ikun <0215.miyatto@gmail.com>
2023-02-28 11:20:15 +0900

    Authentication functionally added to routing

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,25 @@
 const router = require('express').Router();
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 // ! 商品追加機能 GET & POST => /admin/add-product
 // * UI表示
-router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 // * 機能部分
-router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product', isAuth, adminController.postAddProduct);
 
 // ! 商品編集機能 GET & POST => /admin/edit-product
 // * UI表示
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 // * 機能部分
-router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product', isAuth, adminController.postEditProduct);
 
 // ! 商品一覧表示ページ（Admin Products） GET => /admin/products
 // * UI表示
-router.get('/products', adminController.getProducts);
+router.get('/products', isAuth, adminController.getProducts);
 
 // ! 商品削除機能 POST => /admin/delete-product
 // * 機能部分
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 // ! 商品一覧ページ（トップ） GET => /
 // * UI表示
@@ -15,19 +16,19 @@ router.get('/products/:productId', shopController.getProduct);
 
 // ! カートページ GET & POST => /cart
 // * UI表示
-router.get('/cart', shopController.getCart);
+router.get('/cart', isAuth, shopController.getCart);
 // * カート追加機能
-router.post('/cart', shopController.postCart);
+router.post('/cart', isAuth, shopController.postCart);
 
 // ! カート削除機能 POST => /cart-delete-item
 // * カート削除機能
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
 // ! オーダーページ GET => /orders
 // * UI表示
-router.get('/orders', shopController.getOrders);
+router.get('/orders', isAuth, shopController.getOrders);
 // * オーダー機能
-router.post('/create-order', shopController.postOrder);
+router.post('/create-order', isAuth, shopController.postOrder);
 
 
 module.exports = router;
